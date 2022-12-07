9ece615503b6e4252be37834df0429767a2be5ed
EricSegmentationFaultCoreDumpedStewart <ericrobertstewart@gmail.com>
2022-12-07 14:16:14 -0500

    explicitly drop sequence, assign ids manually for init script

--- SQL/DML.sql
+++ SQL/DML.sql
@@ -1,39 +1,29 @@
 DELETE FROM USERCARDS;
 DELETE FROM BOOKAUTHORS;
-
-
 DELETE FROM BOOKORDERS;
 DELETE FROM BOOKGENRES;
 DELETE FROM AUTHOR;
-
 DELETE FROM BOOK;
 DELETE FROM PUBLISHER;
 DELETE FROM USERBOOKSELECTIONS;
 DELETE FROM STOREORDER;
 DELETE FROM STOREUSER;
 
-ALTER SEQUENCE author_authorid_seq MINVALUE 1;
-ALTER SEQUENCE book_isbn_seq MINVALUE 1;
-ALTER SEQUENCE publisher_publisherid_seq MINVALUE 1;
-ALTER SEQUENCE storeorder_ordernumber_seq MINVALUE 1;
-ALTER SEQUENCE storeuser_userid_seq MINVALUE 1;
-
-
 --storeuser
-INSERT INTO STOREUSER (fName,lName,address,email,phoneNumber) values ('Test','User 1','123 Example Street','user1@gmail.com','(123)-456-7890'), 
-('Test','User 2','124 Example Street','user2@gmail.com','(123)-456-7891');
+INSERT INTO STOREUSER (userID,fName,lName,address,email,phoneNumber) values (1,'Test','User 1','123 Example Street','user1@gmail.com','(123)-456-7890'), 
+(2,'Test','User 2','124 Example Street','user2@gmail.com','(123)-456-7891');
 
 --usercard
 INSERT INTO UserCards (userID,cardHolderName,cardNumber,expiryDate,securityCode) values (1,'Test User 1',1234567890,'2024-11-10',123);
 
 --author
-INSERT INTO AUTHOR (fName,lName) values ('Jolkien Rolkien Rolkien','Tolkien');
+INSERT INTO AUTHOR (authorID,fName,lName) values (1,'Jolkien Rolkien Rolkien','Tolkien');
 
 --publisher
-INSERT INTO Publisher (name,address,email,phoneNumber) values ('Random Book Publishing Co.','734 Random Street','contact@rbps.com','0118 999 881 999 119 725 3');
+INSERT INTO Publisher (publisherID,name,address,email,phoneNumber) values (1,'Random Book Publishing Co.','734 Random Street','contact@rbps.com','0118 999 881 999 119 725 3');
 
 --book
-INSERT INTO Book (name, numberOfPages, price, commission, stock, publisherID) values ('Lord of the Hobbit of the Return of the Rings',87934,149.99,0.05,10,1);
+INSERT INTO Book (isbn,name, numberOfPages, price, commission, stock, publisherID) values (1,'Lord of the Hobbit of the Return of the Rings',87934,149.99,0.05,10,1);
 
 --bookauthor
 INSERT INTO bookauthors (authorID,isbn) values (1,1);
@@ -45,7 +35,7 @@ INSERT INTO bookgenres (genre,isbn) values ('Fantastical',1);
 INSERT INTO userbookselections (userID,isbn,quantity) values (1,1,3);
 
 --storeorder
-INSERT INTO storeorder (orderNumber,shippingAddress,courier,deliveryStatus,locationInTransit,dtime,userID) values (DEFAULT,'321 Avenue Street','Courier Courier Services',false,'Warehouse','2022-12-12',1);
+INSERT INTO storeorder (orderNumber,shippingAddress,courier,deliveryStatus,locationInTransit,dtime,userID) values (1,'321 Avenue Street','Courier Courier Services',false,'Warehouse','2022-12-12',1);
 
 --bookorder
 INSERT INTO bookorders (orderNumber,isbn,quantity) values (1,1,2);
